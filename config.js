7cd53c479b31b8c5c34b2e605516ccb6f658636c
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-12-29 16:55:54 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6g_nGGDZjNnBQbtt5f4TkOnV4pFCVu-A",
+  authDomain: "redesocial-eb586.firebaseapp.com",
+  databaseURL: "https://redesocial-eb586-default-rtdb.firebaseio.com",
+  projectId: "redesocial-eb586",
+  storageBucket: "redesocial-eb586.appspot.com",
+  messagingSenderId: "894102143599",
+  appId: "1:894102143599:web:2681a2432910f4e0ea039a"
+  };
